c128d18670cbac37452c77238ee46d888fb4c238
Addy Naik <anaik@vibrenthealth.com>
2022-12-10 11:42:59 -0500

    dont update scripts

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -94,36 +94,3 @@ if [[ $skipPrompts == true ]] || [[ $response =~ ^[yY].*$ ]]; then
 else
 	echo cancelled publisher update
 fi
-
-if [[ $skipPrompts != true ]]; then
-    message="Update scripts? (enter 'y' or 'Y' to continue, any other key to cancel)?"
-    read -r -p "$message" response
-  fi
-
-if [[ $skipPrompts == true ]] || [[ $response =~ ^[yY].*$ ]]; then
-  echo "Downloading most recent scripts "
-
-  curl -L $update_bat_url -o /tmp/_updatePublisher.new
-  cp /tmp/_updatePublisher.new _updatePublisher.bat
-  rm /tmp/_updatePublisher.new
-
-  curl -L $gen_bat_url -o /tmp/_genonce.new
-  cp /tmp/_genonce.new _genonce.bat
-  rm /tmp/_genonce.new
-
-  curl -L $gencont_bat_url -o /tmp/_gencontinuous.new
-  cp /tmp/_gencontinuous.new _gencontinuous.bat
-  rm /tmp/_gencontinuous.new
-
-  curl -L $gencont_sh_url -o /tmp/_gencontinuous.new
-  cp /tmp/_gencontinuous.new _gencontinuous.sh
-  rm /tmp/_gencontinuous.new
-
-  curl -L $gen_sh_url -o /tmp/_genonce.new
-  cp /tmp/_genonce.new _genonce.sh
-  rm  /tmp/_genonce.new
-
-  curl -L $update_sh_url -o /tmp/_updatePublisher.new
-  cp /tmp/_updatePublisher.new _updatePublisher.sh
-  rm /tmp/_updatePublisher.new
-fi
